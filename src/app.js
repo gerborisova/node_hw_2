b447970e9286782a68d428b153641e687acabf62
Gery <gerborisova@gmail.com>
2020-01-23 11:22:23 +0200

    Final changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 
 import Joi from 'joi';
 import express from 'express';
+import shortid from 'shortid';
 
 const app = express();
 app.use(express.json());
-
 
 const users = [];
 
@@ -15,36 +15,36 @@ app.get('/users', (req, res) => {
 
 // Returns the given user if existing
 app.get('/users/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
+    const user = users.find(u => u.id === req.params.id);
     if (!user) res.status(404).send('The user with the given ID was not found');
     res.send(user);
 });
 
-
 // Deletes the user with this id if found and isn't already deleted
 app.delete('/users/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
-     if (!user) {
-        res.status(404).send('The user with the given ID was not found');
+    const user = users.find(u => u.id === req.params.id);
+    if (!user) {
+        return res.status(404).send('The user with the given ID was not found');
     }
     if (user.isDeleted === true) {
-        res.status(400).send('This user was already deleted');
+        return res.status(400).send('This user was already deleted');
     }
-   
+
     user.isDeleted = true;
     res.send(user);
 });
+
 // Creates user if not alread existing
 app.post('/users', (req, res) => {
-    const result = userExists(req.body.login);
-    if (result === false) {
+    const existingUser = userExists(req.body.login);
+    if (existingUser === false) {
         const { error } = validateUser(req.body);
         if (error) {
             res.status(400).send(error.details[0].message);
             return;
         }
         const user = {
-            id: users.length + 1,
+            id: shortid.generate(),
             login: req.body.login,
             password: req.body.password,
             age: req.body.age,
@@ -56,48 +56,43 @@ app.post('/users', (req, res) => {
         res.status(400).send('This user already exists');
     }
 });
+
 // Filtered by loginSubstring and returns limit number of users
 app.get('/suggest', (req, res) => {
     const substring = req.query.loginSubstring || '';
     const limit = req.query.limit || 5;
-
     const filteredUsers = users.filter(user => user.login.indexOf(substring) !== -1);
     filteredUsers.sort((a, b) => a.login.localeCompare(b.login));
     const sliced = filteredUsers.slice(0, limit);
-
     res.send(sliced);
 });
 
-// Edit existing user 
-
-app.put('/users/:id', (req,res)=>{
-    const user = users.find(u => u.id === parseInt(req.params.id));
-     if (!user) {
+// Edit existing user
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+    if (!user) {
         res.status(404).send('The user with the given ID was not found');
-    }else{ 
-        if(req.body){
+    } else {
+        if (req.body) {
             const { error } = validateUser(req.body);
             if (error) {
                 res.status(400).send(error.details[0].message);
                 return;
             }
-            user.id= user.id,
-            user.login= req.body.login || user.login,
-            user.password= req.body.password || user.password,
-            user.age= req.body.age || user.age,
-            user.isDeleted= user.isDeleted
+            user.id = user.id,
+            user.login = req.body.login || user.login,
+            user.password = req.body.password || user.password,
+            user.age = req.body.age || user.age,
+            user.isDeleted = user.isDeleted;
         }
         res.send(user);
     }
-
-
-
-})
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
-function validateUser(user) {
+export function validateUser(user) {
     const schema = {
         login: Joi.string().required(),
         password: Joi.string().regex(/.*?(?:[a-z].*?[0-9]|[0-9].*?[a-z]).*?/).required(),
@@ -106,12 +101,13 @@ function validateUser(user) {
     return Joi.validate(user, schema);
 }
 
-function getActiveUsers() {
+export function getActiveUsers() {
     const activeUsers = users.filter(user => user.isDeleted === false);
     return activeUsers;
 }
 
-function userExists(login) {
+export function userExists(login) {
     const existing = getActiveUsers().filter(user => user.login === login);
     return existing.length > 0;
 }
+
